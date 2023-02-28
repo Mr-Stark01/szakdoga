e1d69caf32338b145845678d7831eb12ab45289e
Daniel_Gyuraki <daniel.gyuraki@case-solvers.com>
2023-02-28 15:19:29 +0100

    commit things that shouldn't be and map render

--- core/src/com/szakdoga/game/screens/GameScreen.java
+++ core/src/com/szakdoga/game/screens/GameScreen.java
@@ -1,7 +1,9 @@
 package com.szakdoga.game.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,6 +11,7 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.szakdoga.game.TowerDefence;
 import com.szakdoga.game.network.DTO.Client;
 import java.util.concurrent.ExecutorService;
@@ -37,18 +40,20 @@ public class GameScreen extends ScreenAdapter {
         bg = new Texture("menu/start_menu.png");
         //Importing and creating map
         TmxMapLoader loader = new TmxMapLoader();
-        map = loader.load("maps/map_01.tmx");
+        map = loader.load("maps/map.tmx");
         tileyLayer = (TiledMapTileLayer) map.getLayers().get(0);
         scale = (float) tileyLayer.getTileWidth();
         renderer = new OrthogonalTiledMapRenderer(map, 1 / scale);
         camera = new OrthographicCamera();
         renderer.setView(camera);
+        ScreenUtils.clear(1, 0, 0, 1);
     }
     @Override
     public void render(float delta){
-        batch.begin();
-        batch.draw(bg,0,0,game.screenWidth,game.screenHeight);
-        batch.end();
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        renderer.setView(camera);
         renderer.render();
+        game.batch.setProjectionMatrix(camera.combined);
     }
 }
